b7f1445dcbabc780c812aa79ad239d8fa3a63513
owenreal <162478543+owenreal@users.noreply.github.com>
2025-05-06 15:26:49 -0500

    5/6 item pickups, activating item function DONE

--- test_item.gd
+++ test_item.gd
@@ -3,6 +3,6 @@ extends CSGBox3D
 @onready var player = get_tree().get_first_node_in_group("Player")
 @onready var data = player.data
 
-func interact():
+func activate():
 	data.hunger = clamp(data.hunger + 30, 0, data.max_hunger)
 	queue_free()
