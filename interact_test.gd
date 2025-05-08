2ec6dbac39ceb414b698e81b1cbbdd181c36862a
owenreal <162478543+owenreal@users.noreply.github.com>
2025-05-08 09:38:22 -0500

    5/8 working dialogue system, change visuals later

--- interact_test.gd
+++ interact_test.gd
@@ -3,6 +3,14 @@ extends CSGBox3D
 @onready var player = get_tree().get_first_node_in_group("Player")
 @onready var data = player.data
 
+@export var dialogue = [
+	"debase cube mode",
+	"you wouldn't get it",
+	"you're not even a cube",
+	"NO DIALOGUE"
+]
+
 func interact():
-	data.hunger = clamp(data.hunger + 30, 0, data.max_hunger)
-	queue_free()
+	#data.hunger = clamp(data.hunger + 30, 0, data.max_hunger)
+	#queue_free()
+	player.do_dialogue(dialogue, 0)
