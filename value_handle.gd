6f36e356418fccfa6d8a71bb1da860bbe0949b1d
owenreal <162478543+owenreal@users.noreply.github.com>
2025-05-19 09:38:07 -0500

    5/19 value testing, works but doesn't reset

--- value_handle.gd
+++ value_handle.gd
@@ -1,9 +1,26 @@
 extends Node
 
 @onready var player = get_tree().get_first_node_in_group("Player")
+@onready var value = player.data.value
+
+var values = {
+	"killing": [2, false],
+	"starving": [30, false],
+	"falling": [60, false],
+}
+
+func value_reset():
+	values.killing[1] = false
 
 func _process(delta: float) -> void:
 	player = get_tree().get_first_node_in_group("Player")
 	if player != null:
-		if player.data.value.kills >= 2:
-			player.take_damage(1000, "visceral")
+		if player.data.STATE == "dead":
+			value_reset()
+		if value.kills >= values.killing[0] and not values.killing[1]:
+			player.data.values.append("killing")
+			var value_path = load("res://values/killing.tscn")
+			var value_scene = value_path.instantiate()
+			player.add_child(value_scene)
+			value_scene.initialize()
+			values.killing[1] = true
---
+++ values/killing.gd
@@ -0,0 +1,15 @@
+extends BaseValue
+
+func _ready() -> void:
+	initialized = false
+
+func initialize():
+	if not initialized:
+		player.drop_item(player.data.left)
+		player.drop_item(player.data.right)
+		player.value_box.text = value_name
+		player.value_box.visible = true
+		await get_tree().create_timer(5).timeout
+		player.value_box.visible = false
+		player.value_box.text = ""
+		initialized = true
