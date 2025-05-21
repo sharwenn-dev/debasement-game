ab6a1a23eb5802c6d0a3230bda553fcecb3be2f8
owenreal <162478543+owenreal@users.noreply.github.com>
2025-05-21 15:26:36 -0500

    5/21 added poi scene

---
+++ poi.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+@onready var spawnable_folder = "res://spawnables/"
+
+var chosen_folder = null
+var chosen_spawnable = null
+
+# choosable folders in dictionary
+
+# items for each folder or very well designed for loop
